e41e5a941af38d5bf409d072bcc8f4e09ec127c3
Bogdan <Faldarknashamir@gmail.com>
2017-06-08 18:10:18 +0300

    first commit, working on DataReader

---
+++ AoCday3/FileReader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AoCday3
+{
+    public class FileReader
+    {
+
+        private string _filepath = @"D:\input data\AoCday3.txt";
+
+        public string[] ReadFromFile()
+        {
+
+            string[] stringArray = File.ReadAllLines(_filepath);
+
+            //int[] intArray = Array.ConvertAll(stringArray, int.Parse);
+
+            return stringArray;
+
+        }
+
+        public List<int[]> arrayParser(string[] inputArray)
+        {
+            List<int[]> parsedArrays = new List<int[]>();
+
+            foreach (var line in inputArray)
+            {
+                string[] stringArray = Regex.Split(line, @"\s");
+                
+
+                int[] intArray =  Array.ConvertAll(stringArray, int.Parse);
+                parsedArrays.Add(intArray);
+            }
+            return parsedArrays;
+        }
+
+    }
+}
---
+++ AoCday3Tests/FileReaderTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AoCday3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCday3.Tests
+{
+    [TestClass()]
+    public class FileReaderTests
+    {
+        [TestMethod()]
+        public void ReadFromFileTestShouldReturnIntArray()
+        {
+
+            //given
+            FileReader reader = new FileReader();
+
+            //when
+            string[] readedArray = reader.ReadFromFile();
+            //then     
+            Assert.IsInstanceOfType(readedArray, typeof(string[]), "something went wrong, wrong arrayType");
+        }
+
+        [TestMethod()]
+        public void arrayParserTestShouldReturnListWithArrays()
+        {
+            //
+            FileReader reader = new FileReader();
+
+            //
+            List<int[]> intList = reader.arrayParser(reader.ReadFromFile());
+            //
+            Assert.IsInstanceOfType(intList, typeof(int[]), "wrong type");
+            Assert.IsTrue(intList.Any(), "sry, List is empty");
+        }
+    }
+}
